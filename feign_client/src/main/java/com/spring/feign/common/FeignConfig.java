0738733a341c73ab0a822b3880fd2f046e958c2f
zhangmengc <190933370@qq.com>
2018-12-13 15:44:57 +0800

    feign 使用规范

--- feign_client/src/main/java/com/spring/feign/common/FeignConfig.java
+++ feign_client/src/main/java/com/spring/feign/common/FeignConfig.java
@@ -21,39 +21,36 @@ public class FeignConfig {
 
     @Bean
     public ErrorDecoder errorDecoder() {
-        return new ErrorDecoder() {
-            @Override
-            public Exception decode(String methodKey, Response response) {
-                try {
-                    if (response.body() != null) {
-                        String body = Util.toString(response.body().asReader());
-                        JSONObject jsonObject = JSONObject.parseObject(body);
-                        log.error("后端报错:{}", jsonObject);
-                        // 判断服务器未补获的异常
-                        Integer status = jsonObject.getInteger("status");
+        return (methodKey, response) -> {
+            try {
+                if (response.body() != null) {
+                    String body = Util.toString(response.body().asReader());
+                    JSONObject jsonObject = JSONObject.parseObject(body);
+                    log.error("methodKey:{}, response:{}", methodKey, jsonObject);
+                    // 判断服务器未补获的异常
+                    Integer status = jsonObject.getInteger("status");
 
-                        // 判断自定义异常 兼容以前版本（后续新代码不建议server端直接抛异常给前端）
-                        Boolean success = jsonObject.getBoolean("success");
-                        if (status != null) {
-                            if (status == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                                // 封装成CustomHystrixBadRequestException不进入fallback
-                                return CustomHystrixBadRequestException
-                                        .buildFailException(status.toString(), jsonObject.getString("error"));
-                            } else {
-                                return FeignRemoteException.buildFailException(body);
-                            }
-                        } else if(success != null && !success) {
-                            ResponseMsg msg = JSONObject.parseObject(body, ResponseMsg.class);
+                    // 判断自定义异常 兼容以前版本（后续新代码不建议server端直接抛异常给前端）
+                    Boolean success = jsonObject.getBoolean("success");
+                    if (status != null) {
+                        if (status == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                            // 封装成CustomHystrixBadRequestException不进入fallback
                             return CustomHystrixBadRequestException
-                                    .buildFailException(msg.getErrorCode(), msg.getMessage());
+                                    .buildFailException(status.toString(), jsonObject.getString("error"));
+                        } else {
+                            return FeignRemoteException.buildFailException(body);
                         }
+                    } else if(success != null && !success) {
+                        ResponseMsg msg = JSONObject.parseObject(body, ResponseMsg.class);
+                        return CustomHystrixBadRequestException
+                                .buildFailException(msg.getErrorCode(), msg.getMessage());
                     }
-                } catch (Exception e) {
-                    log.error("处理Feign的异常信息失败！", e);
                 }
-                // 处理其他异常情况
-                return FeignRemoteException.buildFailException("远程服务调用失败！");
+            } catch (Exception e) {
+                log.error("methodKey:{}", methodKey, e);
             }
+            // 处理其他异常情况
+            return FeignRemoteException.buildFailException("远程服务调用失败！");
         };
     }
 
